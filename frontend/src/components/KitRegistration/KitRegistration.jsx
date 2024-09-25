f86cb1c458bd46a8346fa994f3980a5c1fa879d7
cyberbots-123 <cyberbotsdb@gmail.com>
2024-09-25 10:06:21 +0530

    Update KitRegistration.jsx

--- frontend/src/components/KitRegistration/KitRegistration.jsx
+++ frontend/src/components/KitRegistration/KitRegistration.jsx
@@ -108,7 +108,7 @@ const KitRegistration = () => {
       const priceValue = formData.price.replace(/[^\d.-]/g, ''); // Remove non-numeric characters
       console.log('Submitting form data:', { ...formData, price: priceValue }); // Log form data
       try {
-        const response = await fetch('http://localhost:4000/api/register', {
+        const response = await fetch('https://api.cyberbots.in/api/register', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
